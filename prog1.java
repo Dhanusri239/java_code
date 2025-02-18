ce8345293a15136fe193acc7e730a55dfd05d2db
Dhanusri239 <sdhanu946@gmail.com>
2025-02-18 22:24:48 +0530

    prog2.java file is added

---
+++ prog1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class prog1{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        int age=sc.nextInt();
+        System.out.println(name);
+        System.out.println(age);
+
+    }
+}
